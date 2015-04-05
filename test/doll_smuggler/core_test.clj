9d77f26aee67309ba32d903ab13926134035b848
Matt Rozema <mattrozema89@gmail.com>
2015-04-05 21:42:11 +0100

    Added unit test for the find-dolls function using sample data

--- test/doll_smuggler/core_test.clj
+++ test/doll_smuggler/core_test.clj
@@ -3,7 +3,8 @@
             [doll-smuggler.core :refer :all]))
 
 (def sample-dolls 
-  [{ :name "luke",    :weight 9,   :value 150} { :name "anthony",  :weight 13, :value 35 }  
+  ( list
+   { :name "luke"    :weight 9   :value 150} { :name "anthony"  :weight 13 :value 35 }  
    { :name "candice" :weight 153 :value 200} { :name "dorothy"  :weight 50 :value 160} 
    { :name "puppy"   :weight 15  :value 60 } { :name "thomas"   :weight 68 :value 45 }  
    { :name "randal"  :weight 27  :value 60 } { :name "april"    :weight 39 :value 40 } 
@@ -13,20 +14,33 @@
    { :name "uma"     :weight 73  :value 40 } { :name "grumpkin" :weight 42 :value 70 } 
    { :name "dusty"   :weight 43  :value 75 } { :name "grumpy"   :weight 22 :value 80 }  
    { :name "eddie"   :weight 7   :value 20 } { :name "tory"     :weight 18 :value 12 } 
-   { :name "sally"   :weight 4   :value 50 } { :name "babe"     :weight 30 :value 10 }])
+   { :name "sally"   :weight 4   :value 50 } { :name "babe"     :weight 30 :value 10 }))
+
+
+(def sample-dolls-answer
+  (list
+  {:name "sally"    :weight 4   :value 50 } 
+  {:name "eddie"    :weight 7   :value 20 }
+  {:name "grumpy"   :weight 22  :value 80 } 
+  {:name "dusty"    :weight 43  :value 75 }
+  {:name "grumpkin" :weight 42  :value 70 } 
+  {:name "marc"     :weight 11  :value 70 } 
+  {:name "randal"   :weight 27  :value 60 } 
+  {:name "puppy"    :weight 15  :value 60 } 
+  {:name "dorothy"  :weight 50  :value 160} 
+  {:name "candice"  :weight 153 :value 200} 
+  {:name "anthony"  :weight 13  :value 35 } 
+  {:name "luke"     :weight 9   :value 150}))
+
 
 (comment "Sample data set from problem description:
           https://github.com/micahalles/doll-smuggler")
-(deftest yaml-read_sample-data
+(deftest process-sample-data
   (testing 
-    "Test the algorithm against the sample data"
-    
-  (is (= 
-        (find-dolls sample-dolls 400)
-        [["sally" 4 50] ["eddie" 7 20] ["grumpy" 22 80] ["dusty" 43 75] 
-         ["grumpkin" 42 70] ["marc" 11 70] ["randal" 27 60] ["puppy" 15 60] 
-         ["dorothy" 50 160] ["candice" 153 200] ["anthony" 13 35] ["luke" 9 150]]))))
-
+    "Test the find-dolls algorithm against the sample data"
+    (let [[value indexes] (find-dolls (-> sample-dolls count dec) 400 sample-dolls)]
+      (is (= (set indexes) (set [0 1 2 3 4 6 10 15 16 17 18 20])))
+      (is (= value 1030)))))
 
 
 
