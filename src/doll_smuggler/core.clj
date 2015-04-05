f5ee0b418fffaf74b05792fad9775667169614e0
Matt Rozema <mattrozema89@gmail.com>
2015-04-05 22:20:56 +0100

    Implemented core functionality for problem. All tests passing

--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,20 +1,52 @@
 (ns doll-smuggler.core
   (:require [clj-yaml.core :as yaml]
-            [doll-smuggler.doll_reader :as reader])
+            [doll-smuggler.doll_reader :as reader]
+            [clojure.pprint :as pprint :refer [pprint print-table]])
   (:gen-class))
 
-;; Define the doll structure
-(defstruct doll :name :weight :value)
+(use '[clojure.string :only [join]])
+(use 'clojure.inspector)
 
-;(def dolls (vec (map #(apply struct doll %)
-;                     (partition 3 doll-data))))
+(declare m-find-dolls)
 
-(defn find-dolls [idx weight]
-  (printf "Hello!"))
+(defn find-dolls [idx weight dolls]
+   (cond
+    (< idx 0) [0 []]
+    (= weight 0) [0 []]
+    :else
+    (let [{doll-weight :weight doll-value :value} (nth dolls idx)]
+      (if (> doll-weight weight)
+        (m-find-dolls (dec idx) weight dolls)
+        (let [[vn sn :as no]  (m-find-dolls (dec idx) weight dolls)
+              [vy sy :as yes] (m-find-dolls (dec idx) (- weight doll-weight) dolls)]
+          (if (> (+ vy doll-value) vn)
+            [(+ vy doll-value) (conj sy idx)]
+            no))))))
+ 
+(def m-find-dolls (memoize find-dolls))
+
+(defn total-weight
+  [dolls]
+  (get (apply merge-with + (for [x dolls] (select-keys x [:weight]))) :weight))
+
+(defn filter-by-index 
+  [coll idx]
+  (map (partial nth coll) idx))
+
+(defn print-results
+  [results]
+  (print-table [:name :weight :value] (sort-by :name results)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [doll-data (reader/read-yaml "resources/test.yml")]
-    (find-dolls (-> doll-data count dec) 400)))
+    (let [[value indexes] (find-dolls (-> doll-data count dec) 400 doll-data)
+      names (filter-by-index doll-data indexes)]
+      (println "Dolls to pack:")
+      (print-results names)
+      (println "-------------------------------")
+      (println "Total Value:" value)
+      (println "Total Weight:" (total-weight names))
+      (inspect-table names))))
 
