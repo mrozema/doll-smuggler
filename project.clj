7c5eb42f786efbca380a96e459389d1c4dc356e6
Matt Rozema <mattrozema89@gmail.com>
2015-04-06 01:10:51 +0100

    Added CLI interface

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-yaml "0.4.0"]]
+                 [clj-yaml "0.4.0"]
+                 [org.clojure/tools.cli "0.3.1"]]
   :main ^:skip-aot doll-smuggler.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/doll_smuggler/core.clj
+++ src/doll_smuggler/core.clj
@@ -1,13 +1,53 @@
 (ns doll-smuggler.core
   (:require [clj-yaml.core :as yaml]
             [doll-smuggler.doll_reader :as reader]
-            [clojure.pprint :as pprint :refer [pprint print-table]])
+            [clojure.string :as string]
+            [clojure.pprint :as pprint :refer [pprint print-table]]
+            [clojure.inspector :refer [inspect-table]]
+            [clojure.java.io :refer [as-file]]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(use '[clojure.string :only [join]])
-(use 'clojure.inspector)
-
 (declare m-find-dolls)
+
+(def cli-options
+  [
+   ["-w" "--weight WEIGHT" "Maximum total weight (in lbs) for the dolls"
+    :id :max-weight
+    :default 400
+    :default-desc "Default: 400 lbs"
+    :parse-fn #(Integer/parseInt %1)
+    :validate [#(> %1 0) "Must be a positive number"]]
+   ["-f" "--file FILEPATH" "Input file path (YAML). E.g. drug-stuff.yaml"
+    :id :file
+    :default "resources/doll-input.yml"
+    :default-desc "Default: doll-input.yml"
+    :validate [#(.exists (as-file %1)) "File does not exist!"]]
+   [nil "--chart" "Create a graphical chart of the calculated data"
+    :id :chart
+    :default-desc "Displays the results in a nice GUI"]
+   ["-h" "--help"]])
+
+(defn usage 
+  [options-summary]
+  (->> ["Welcome, Drug Smuggler."
+        "I will help you find the optimum doll set for your mule!"
+        ""
+        "Usage: doll-smuggler [options]"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Please refer to the README for more information."]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
 
 (defn find-dolls [idx weight dolls]
    (cond
@@ -38,15 +78,24 @@
   (print-table [:name :weight :value] (sort-by :name results)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Parse command line options and calculate the doll set"
   [& args]
-  (let [doll-data (reader/read-yaml "resources/test.yml")]
-    (let [[value indexes] (find-dolls (-> doll-data count dec) 400 doll-data)
-      names (filter-by-index doll-data indexes)]
-      (println "Dolls to pack:")
-      (print-results names)
-      (println "-------------------------------")
-      (println "Total Value:" value)
-      (println "Total Weight:" (total-weight names))
-      (inspect-table names))))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;; Handle help and error conditions
+    (cond
+      (:help options) (exit 0 (usage summary))
+      (not= (count arguments) 0) (exit 1 (usage summary))
+      errors (exit 1 (error-msg errors)))
+    ;; Run the program
+    (let [doll-data (reader/read-yaml (get options :file))]
+      (let [[value indexes] (find-dolls (-> doll-data count dec) (get options :max-weight) doll-data)
+            names (filter-by-index doll-data indexes)]
+        (println "Dolls to pack:")
+        (print-results names)
+        (println "-------------------------------")
+        (println "Total Value:" value)
+        (println "Total Weight:" (total-weight names))
+        (if (get options :chart) 
+          (inspect-table names) 
+          (exit 0 "Goodbye and Good Luck!"))))))
 
